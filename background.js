db3d5aa5d20079c25e7a3860b192036061058d83
Narin Luangrath <narin.luangrath@gmail.com>
2018-05-19 18:27:15 -0400

    iss1 - Add listeners, refactor tab update code

--- background.js
+++ background.js
@@ -1,98 +1,79 @@
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log("Tabs extension is running...");
-  });
-});
+/* Add listeners for tabs */
+
+const tabs = chrome.tabs 
+tabs.onCreated.addListener( function( tab ) {
+	updateTab( tab )
+})
+tabs.onUpdated.addListener( function( tabId, changeInfo, tab ) {
+	updateAllTabs()
+})
+tabs.onMoved.addListener( function( tabId, moveInfo ) {
+	updateAllTabs()
+})
+tabs.onDetached.addListener( function( tabId, detachInfo ) {
+	updateAllTabs()
+})
+tabs.onAttached.addListener( function( tabId, attachInfo ) {
+	updateAllTabs()
+})
+tabs.onRemoved.addListener( function( tabId, removeInfo ) {
+	updateAllTabs()
+})
+
 
 /**
-01. Tabs Chrome Extension
-Add a number in front of tab titles. Then you can easily use quick hot keys to switch to tab. e.g. Ctrl+7 / CMD+7
-Created by AK Solutions - AK Sol Ltd. United Kingdom.
-Under GPL license
+ * Add numeric prefix to tab title (i.e. '4. Facebook - Login')
+ * according to tab's position relative to other tabs. If the 
+ * prefix already exists, update it to make sure it's accurate.
+ * @param {Tab} tab - a [Chrome API Tab object](https://developer.chrome.com/extensions/tabs#type-Tab)  
+ */
 
-Inspiration by Tab Number extension
-https://chrome.google.com/webstore/detail/tab-number/dnndfognablbihihgnilabcegjjkiekj
+function updateTab( tab ) {
+	const { id, index, url, title } = tab
 
-**/
-
-/* Listen for when a tab has changed position */
-chrome.tabs.onMoved.addListener(function(id) {
-  nTabsUpdate(true,null);
-});
-
-/* Listen for when a tab has been removed */
-chrome.tabs.onRemoved.addListener(function(id) {
-  nTabsUpdate(true,null);
-});
-
-/* Listen for when a tab has an updated title */
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  nTabsUpdate(false,tab);
-});
-
-var nTabsUpdate = function(all_tabs, details) {
-
-	// process all tabs one by one
-	if (all_tabs) {
-	
-	  chrome.tabs.query({}, function(tabs) {
-	  	var one_tab;
-		for (var i = 0; one_tab = tabs[i]; i++) {
-		  nTabsUpdate(false, one_tab);
-		}
-	  });
-	} else {
-
-		// tab details
-		var tab_id = details.id;
-		var tab_title = details.title;
-		var tab_url = details.url;
-
-		if ( tab_title.substr( 0, 4 ) === 'http' ) return
-
-		// console.log("||| details = " + details.title + "");
-		
-		// console.log("1. tab_title = " + tab_title + "");
-		
-		// other variables
-		var index = details.index;
-		var pad  = '';
-
-		// if we found a . at third character of title
-		// it would not work on 100th tab, but who has 100 tabs? not the normal ones. Those chosen ones can roll their own extension
-		if (tab_title && tab_title.indexOf('. ') == 2) {
-			tab_title = tab_title.substr(4);
-		}
-
-		// console.log("2. tab_title = " + tab_title + "");
-
-		// add leading zero where needed
-		if (index<9) { 
-			pad = '0'; 
-		}
-
-		// add the index
-		tab_title = pad + (index + 1) + '. ' + tab_title;
-		
-		// console.log("3. tab_title = " + tab_title + "");
-
-		// apply the new title if this is not extension gallery
-		if (tab_url.substr(0,35)=='https://chrome.google.com/webstore/' || tab_url.substr(0,6)=='chrome') {
-			// this is ridiculous, a ! does not work above, so here is a point less block of if to make use of else instead 
-		} else {
-			try {
-				chrome.tabs.executeScript(tab_id,
-				{
-					code : "document.title = '" + tab_title + "';"
-				  }
-				);
-				//console.log("document.title = '" + tab_title + "';");
-			} catch(e) {}
-		}
+	if ( url.startsWith( 'chrome://' ) ) {
+		// not allowed to modify browser settings pages
+		return 
 	}
-};
 
+  /* logic for redoing title with numeric prefix */
+	const prefix = /[0-9]+. /g
+	const hasPrefix = prefix.exec( title )
+	let newTitle = `${index + 1}. `
+	if ( hasPrefix && hasPrefix[0] && hasPrefix[0] === newTitle ) {
+		return // title is already correctly prefixed
+	} else if ( hasPrefix ) {
+		// title has incorrect prefix (tab moved)
+		newTitle += title.split( prefix )[1]
+	} else {
+		// title has no prefix (new tab)
+		newTitle += title
+	}
 
-/* in the beginning please update all tabs */
-nTabsUpdate(true,null);
+	try {
+		const script = { code : `document.title = '${newTitle}'` }
+		chrome.tabs.executeScript( id, script )
+	} catch ( e ) {
+		console.error( e )
+	}
+}
 
+/**
+ * Get all the Tabs (from all windows)
+ * @param {function} cb - callback function with input `Tab[]` 
+ */
+
+function getAllTabs( cb ) {
+	chrome.tabs.query( {}, cb )
+}
+
+/**
+ * Update all the tabs.
+ */
+
+function updateAllTabs( ) {
+	const cb = tabs => tabs.forEach( updateTab )
+	getAllTabs( cb )
+}
+
+updateAllTabs() // update all tabs on initialization
