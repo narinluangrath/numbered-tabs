d1606eb981554cbe9084c005619a662196f01df6
Narin Luangrath <narin.luangrath@gmail.com>
2020-04-27 21:57:37 -0700

    Finish deploy script

--- deploy.js
+++ deploy.js
@@ -3,28 +3,39 @@ const fs = require('fs');
 const createWebStore = require('chrome-webstore-upload');
 const archiver = require('archiver');
 
-// Create zip file from /src 
-const zipped = fs.createWriteStream(path.resolve(__dirname, 'numbered-tabs-extension.zip'));
-const archive = archiver('zip');
-archive.on('error', err => console.error('Zip file failed', error));
-archive.pipe(zipped);
-archive.directory(path.resolve(__dirname, 'src'), false);
-archive.finalize();
+const logger = console.log;
 
-const webStore = createWebStore({
+const config = {
   extensionId: process.env.EXTENSION_ID,
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
   refreshToken: process.env.REFRESH_TOKEN
-});
+};
+logger("Current config", config)
+const ZIP_FILE_NAME = 'numbered-tabs-extension.zip';
+const webStore = createWebStore(config);
 
-async function uploadAndPublish(webStore, zipFile) {
+// Write zip file from /src 
+const zipped = fs.createWriteStream(path.resolve(__dirname, ZIP_FILE_NAME));
+zipped.on('finish', () => {
+  logger('Zip file created');
+  uploadAndPublish(webStore);
+});
+const archive = archiver('zip');
+archive.on('error', err => console.error('Zip file failed', error));
+archive.pipe(zipped);
+archive.directory(path.resolve(__dirname, 'src'), false);
+// Upload to chrome store after zip file is created
+archive.finalize();
+
+async function uploadAndPublish(webStore) {
+  const zipFile = fs.createReadStream(path.resolve(__dirname, ZIP_FILE_NAME));
+  logger("Uploading...")
   const token = await webStore.fetchToken();
-  const uploadRes = await webStore.uploadExisting(zipFile, token)
+  console.log("Got an access token!", token);
+  const uploadRes = await webStore.uploadExisting(zipFile, token);
+  logger("Finished uploading!", uploadRes);
+  const publishRes = await webStore.publish('default', token);
+  logger("Finished publishing!", publishRes);
 }
 
-// Upload to chrome store after zip file is created
-zipped.on('end', () => {
-  console.log('Zip file created');
-  uploadAndPublish(webStore, zipped);
-});
